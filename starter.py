f59e94d7110fdf2b8c0c78dab28bbd5bfbf88f86
Marvin Liu <marvinliu9@gmail.com>
2016-05-07 17:21:10 +0800

    added some images

--- starter.py
+++ starter.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
 import math
+import itertools
 import matplotlib.pyplot as plt
-from pandas.stats.moments import ewma
-from scipy.optimize import curve_fit
-
 from sklearn.linear_model import LinearRegression
 
 
+# Splits the year_month column into year and month and returns them as integers
 def get_year_month(year_month):
-    ''' splits the year_month column to year and month and returns them in int format'''
     years = []
     months = []
     for y_m in year_month:
@@ -18,10 +16,7 @@ def get_year_month(year_month):
         months.append(int(m))
     return years,months
 
-
 def get_zero_repairs(st_year,end_year):
-    import pandas as pd
-    import itertools
     zero_df = []
     years = list(range(st_year,end_year +1))
     months = list(range(1,13))
@@ -30,18 +25,7 @@ def get_zero_repairs(st_year,end_year):
     zero_df.columns = ['year','month','number_repair']
     return zero_df
 
-
-
-repair_train = pd.read_csv('../data/RepairTrain.csv')
-repair_train['year_sale'],repair_train['month_sale'] = get_year_month(repair_train['year/month(sale)'])
-repair_train['year_repair'],repair_train['month_repair'] = get_year_month(repair_train['year/month(repair)'])
-repair_min_year = repair_train['year_repair'].min()
-repair_max_year = repair_train['year_repair'].max()
-
-cols_requ = ['module_category','component_category','year_repair','month_repair','number_repair']
-cols_groupby = ['module_category','component_category','year_repair','month_repair']
-repair_train_summ = repair_train[cols_requ].groupby(cols_groupby).sum()
-
+# Return repair data for a specific module and component
 def get_repair_complete(module,component):
     zero_repair = get_zero_repairs(repair_min_year,repair_max_year)
     repair_module_component = repair_train[np.logical_and(
@@ -58,50 +42,76 @@ def get_repair_complete(module,component):
 
     return repair_merged[cols_req_final]
 
-
-pred_period = 19
-
-def get_prediction(first_prediction, k, prediction_count):
+# Given an initial prediction first_prediction
+# Apply decay coefficient k for num_preds number of periods 
+def get_prediction(first_prediction, k, num_preds):
     result = [first_prediction]
 
-    for i in range(1, prediction_count):
+    for i in range(1, num_preds):
         result.append(result[-1] * k)
 
     return result
 
-def num_decreases(y):
-    decreases = 0
-    for i in range(1, y):
-        if y.iloc[i] < y.iloc[i - 1]:
-            decreases += 1
+repair_train = pd.read_csv('../data/RepairTrain.csv')
+repair_train['year_sale'],repair_train['month_sale'] = get_year_month(repair_train['year/month(sale)'])
+repair_train['year_repair'],repair_train['month_repair'] = get_year_month(repair_train['year/month(repair)'])
+repair_min_year = repair_train['year_repair'].min()
+repair_max_year = repair_train['year_repair'].max()
 
-    return decreases
-
+cols_requ = ['module_category','component_category','year_repair','month_repair','number_repair']
+cols_groupby = ['module_category','component_category','year_repair','month_repair']
+repair_train_summ = repair_train[cols_requ].groupby(cols_groupby).sum()
 
 output_target = pd.read_csv('../data/Output_TargetID_Mapping.csv')
 submission = pd.read_csv('../data/SampleSubmission.csv')
 
+# Number of predictions to make
+pred_period = 19
 
+c = 0
+# Make predictions for each module-component
 for i in range(0,output_target.shape[0],pred_period):
     module = output_target['module_category'][i]
     category = output_target['component_category'][i]
+    print 'Predicting for ' + str(module) + ' ' + str(category) + '.'
+
+    # Replace all empty fields with 0
+    # Use only the last 8 months to predict
     X = get_repair_complete(module,category).fillna(0).query('year >= 2009 & month >= 5')
+    
+    # Scale first month to 0
     years = X.year.apply(lambda y: (y-2009)*12)
     months = X.month.apply(lambda m: m - 5)
     x = years.astype(int).combine(months, func=lambda x, y: x + y)[:, np.newaxis]
     y = X.number_repair
 
+    # Take the log of (number of repairs + 1) for the last 8 months
+    # Fit a line through the resulting datapoints
     lr = LinearRegression()
     lr.fit(x, y.apply(lambda y: np.log(y + 1)))
 
+    # Calculate decay coefficient k
     k = lr.coef_[0] if lr.coef_[0] <= 0 else np.log(0.91)
     k = np.exp(k)
 
-    decreases = num_decreases(y)
-    first_prediction = y.iloc[-1] if decreases > 3 else np.average(y[-3:])
-    print (first_prediction, k)
+    # Initial month (01/2010) prediction is same as previous month (12/2009)
+    first_prediction = y.iloc[-1]
 
+    if c == 3:
+        plt.plot(x, y, 'ko')
+        plt.show()
+
+    # Make predictions
     submission['target'][i:i+pred_period] = get_prediction(first_prediction, k, pred_period)
+    c += 1
 
-submission.to_csv('beat_benchmark_2.csv',index=False)
-print('submission file created')
+# submission.to_csv('submission.csv',index=False)
+
+print 'submission.csv created.'
+
+
+
+
+
+
+
